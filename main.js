a03a10560bf852f2c0b0b7a7724e177503b80589
Yenou Isaiah <97550820+theyenisa@users.noreply.github.com>
2023-01-11 02:40:38 +0100

    initial upload

---
+++ main.js
@@ -0,0 +1,6 @@
+let type = new Typed(".autoType", {
+    strings: ["Yenisa", "A Programmer", "A Designer", "A Software Developer", "Always Awake 😴😟😜", "Your Buddy!\nLet's connect!"],
+    typeSpeed: 150,
+    backsSpeed: 150,
+    loop: true
+})
